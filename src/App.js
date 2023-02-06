b16e875fa9a5380b0b8ca26dd16db8a99f1293b9
colins1 <maxrozhkov1@gmail.com>
2023-02-06 20:29:59 +0300

    add

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form class="form-inline">
+  <div class="form-group">
+    <label class="sr-only" for="inputEmail">Email</label>
+    <input type="email" class="form-control" id="inputEmail" placeholder="Email"/>
+  </div>
+  <div class="form-group">
+    <label class="sr-only" for="inputPassword">Пароль</label>
+    <input type="password" class="form-control" id="inputPassword" placeholder="Пароль"/>
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"/> Запомнить</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Войти</button>
+</form>
   );
 }
 
